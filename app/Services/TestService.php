ea100eb8b1f8b428b8848dafe016ab15e8c60c7c
Duc Vu <vuducsaobien95@gmail.com>
2025-04-22 11:07:08 +0700

    Flow code API, Error Handling, transaction, rollback Database

--- app/Services/TestService.php
+++ app/Services/TestService.php
@@ -4,6 +4,10 @@ namespace App\Services;
 
 use App\Repositories\TestRepository;
 use App\Enum\DatabaseExceptionTypesEnum;
+use App\Exceptions\BusinessException;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+
 class TestService
 {
     private $repository;
@@ -36,10 +40,26 @@ class TestService
     
     public function testBussinessException()
     {
-        $idNotExist = -99999;
-        $testRecordNotFound = $this->repository->findById($idNotExist);
-        if (!$testRecordNotFound) {
-            throw new \App\Exceptions\BusinessException('BusinessException - Test record not found');
+        DB::beginTransaction();
+        try {
+            // Update record để test rollback DB
+            $id = 1;
+            $this->repository->update($id, ['name' => 'Test Rollback DB']);
+
+            // Test record not found - BusinessException
+            $idNotExist = -99999;
+            $testRecordNotFound = $this->repository->findById($idNotExist);
+            if (!$testRecordNotFound) {
+                throw new BusinessException('BusinessException - Test record not found');
+            }
+
+            // Test division by zero - SystemException
+            // $divisionByZero = 5 / 0;
+            
+            DB::commit();
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            throw $e;
         }
     }
 
